7751c7cd4856a75e9354bc896c8469622cdb45b0
THORNG-VANTHIEV <thorngvanthiev@gmail.com>
2026-08-22 11:54:22 +0700

    Update mineral water images to Dasani for 500ml and 1.5L

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global PRECACHE_URLS */
 importScripts("./precache-manifest.js");
 
-const VERSION = "v1.0.55";
+const VERSION = "v1.0.56";
 const STATIC_CACHE = `emenu-static-${VERSION}`;
 const DATA_CACHE = `emenu-data-${VERSION}`;
 const IMAGE_CACHE = `emenu-images-${VERSION}`;
